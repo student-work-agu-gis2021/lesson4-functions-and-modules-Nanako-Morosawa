118011676ed6662cb73a16093808fe49af4ca948
NanakoMorosawa <>
2021-05-07 04:47:46 +0000

    I wrote my name and student number.

--- Exercise_4_problem_1.py
+++ Exercise_4_problem_1.py
@@ -19,6 +19,7 @@
 # - Adding comments in your code and a docstring that explains how to use your `fahr_to_celsius` function (i.e., you should write the purpose of the function, parameters, and returned values)
 
 # YOUR CODE HERE
+def fahr_to_celsius()
 
 # ### Problem 1 tests
 # 
