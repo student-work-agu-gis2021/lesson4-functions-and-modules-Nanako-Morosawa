faca2b3b1daa38b56b25b8bb5f327ba4680b3e7c
NanakoMorosawa <>
2021-05-09 16:54:20 +0000

    Add comments in my code using a docstring that explains how to use my temp_classifier function.

--- Exercise_4_problem_2.py
+++ Exercise_4_problem_2.py
@@ -26,7 +26,7 @@
 
 # YOUR CODE HERE to define temp_classifier
 def temp_classifier(temp_celsius): 
-  """Create a temp_classifier that classify the temperature and return the classification number"""
+  """Create a temp_classifier that classify the temperature and return the classification number""" 
 
 # Use your newly created function to print the answers to the following questions:
 
